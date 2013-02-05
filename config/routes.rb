5441dc5f3b6b6a19a8ba3282f61002b60299901e
Anton Sizov <a.sizov@dev1team.net>
2013-02-05 11:07:37 +0300

    Add pages controller and home action

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Stui::Application.routes.draw do
+  get "pages/home"
+
   devise_for :users
+  root :to => 'pages#home'
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
