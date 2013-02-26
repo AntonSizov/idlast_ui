bfa55d57cc8094ca84da2c55e9efda85f1f4d229
Anton Sizov <a.sizov@dev1team.net>
2013-02-26 12:59:33 +0300

    Improve db seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,7 +7,17 @@
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
 puts 'SETTING UP DEFAULT USER LOGIN'
-user = User.create! :name => 'Anton Sizov', :admin => true, :email => 'a.sysoff@gmail.com', :password => 'administrator', :password_confirmation => 'administrator'
-puts 'New user created: ' << user.name
-user2 = User.create! :name => 'iKatod', :admin => true, :email => 'ikatod@gmail.com', :password => 'ikatodikatod', :password_confirmation => 'ikatodikatod'
+
+user0 = User.new :name => 'IDLast.com', :email => 'idlast@idlast.com', :password => '987245jo8ug98rt4ji9g8'
+user0.save!
+puts 'New user created: ' << user0.name
+
+user1 = User.new :name => 'Anton Sizov', :email => 'a.sysoff@gmail.com', :password => 'administrator'
+user1.admin = true
+user1.save!
+puts 'New user created: ' << user1.name
+
+user2 = User.new :name => 'iKatod', :email => 'ikatod@gmail.com', :password => 'ikatodikatod'
+user2.admin = true
+user2.save!
 puts 'New user created: ' << user2.name
