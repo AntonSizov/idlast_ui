c94b775e9b6e168d85bcb947e25f7fe6fd7fc0c6
Anton Sizov <a.sizov@dev1team.net>
2013-03-03 17:13:40 +0300

    Add AJAX for comments

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -4,10 +4,15 @@ class CommentsController < ApplicationController
     @article = Article.find(params[:article_id])
     @comment = @article.comments.new(params[:comment])
     @comment.set_user(current_user)
-    if @comment.save
-      redirect_to @article, :notice => 'Comment created!'
-    else
-      redirect_to @article, :error => 'Comment was not created'
+
+    respond_to do |format|
+      if @comment.save
+        format.html { redirect_to(@article, :notice => 'Comment created') }
+        format.js
+      else
+        format.html { redirect_to(@article, :error => 'Comment was not created') }
+        format.js
+      end
     end
   end
 
