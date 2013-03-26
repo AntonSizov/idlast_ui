da2254cfc5b6eaf481d930c9703dc6cb0b005953
Anton Sizov <a.sizov@dev1team.net>
2013-03-26 20:08:16 +0300

    Improve user resource
    Remove unneed actions and views

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,27 +3,8 @@
   #load_and_authorize_resource
   before_filter :authenticate_user!
 
-  def index
-    @title = "All users"
-    @users = User.paginate(:page => params[:page], :per_page => 10)
-  end
-
   def show
-    @user = User.find(params[:id])
-  end
-
-  def new
-    @user = User.new
-    @current_method = "new"
-  end
-
-  def create
-    @user = User.new(params[:user])
-    if @user.save
-      redirect_to(@user, :notice => 'User was successfully created.')
-    else
-      render :action => "new"
-    end
+    @user = User.find(current_user.id)
   end
 
   # http://stackoverflow.com/questions/6607834/rails-3-devise-manually-change-password
