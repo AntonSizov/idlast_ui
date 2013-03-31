ae80d7dbd5e1298de7d48cc0f57695632afe4d27
Anton Sizov <a.sizov@dev1team.net>
2013-03-31 18:53:26 +0300

    Add flash notifications

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,5 +1,7 @@
 module ApplicationHelper
 
+  ALERT_TYPES = [:error, :info, :success, :warning]
+
   def title
     base_title = "IDLast.com"
     if @title.nil?
@@ -18,6 +20,26 @@ module ApplicationHelper
 
   def admin_ns?
     controller.class.name.split("::").first=="Admin"
+  end
+
+  def bootstrap_flash
+    flash_messages = []
+    flash.each do |type, message|
+      # Skip empty messages, e.g. for devise messages set to nothing in a locale file.
+      next if message.blank?
+
+      type = :success if type == :notice
+      type = :error   if type == :alert
+      next unless ALERT_TYPES.include?(type)
+
+      Array(message).each do |msg|
+        text = content_tag(:div,
+                           content_tag(:button, raw("&times;"), :class => "close", "data-dismiss" => "alert") +
+                           msg.html_safe, :class => "alert fade in alert-#{type}")
+        flash_messages << text if message
+      end
+    end
+    flash_messages.join("\n").html_safe
   end
 
 end
