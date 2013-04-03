b2f9c931836d47862925e44da7f32a593bcc15f2
Anton Sizov <a.sizov@dev1team.net>
2013-04-03 12:18:15 +0300

    Generate password randomly in production env

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,18 +6,25 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
+def gen_password
+  return (('0'..'9').to_a + ('a'..'z').to_a + ('A'..'Z').to_a).shuffle[0,20].join
+end
+
 puts 'SETTING UP DEFAULT USER LOGIN'
 
 user0 = User.new :name => 'IDLast.com', :email => 'idlast@idlast.com', :password => '987245jo8ug98rt4ji9g8'
+user0.password = gen_password if Rails.env.production?
 user0.save!
 puts 'New user created: ' << user0.name
 
 user1 = User.new :name => 'Anton Sizov', :email => 'a.sysoff@gmail.com', :password => 'administrator'
+user1.password = gen_password if Rails.env.production?
 user1.admin = true
 user1.save!
 puts 'New user created: ' << user1.name
 
 user2 = User.new :name => 'iKatod', :email => 'ikatod@gmail.com', :password => 'ikatodikatod'
+user2.password = gen_password if Rails.env.production?
 user2.admin = true
 user2.save!
 puts 'New user created: ' << user2.name
