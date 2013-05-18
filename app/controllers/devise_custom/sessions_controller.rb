1905d5080a095325e8180cef46d1b3609b4447bb
Anton Sizov <a.sizov@dev1team.net>
2013-05-18 18:58:28 +0300

    Fix timezone autodetection

--- app/controllers/devise_custom/sessions_controller.rb
+++ app/controllers/devise_custom/sessions_controller.rb
@@ -1,6 +1,8 @@
 class DeviseCustom::SessionsController < Devise::SessionsController
   def create
-    current_user.update_attribute(:timezone, cookies["jstz_time_zone"])
+    if current_user
+      current_user.update_attribute(:timezone, cookies["jstz_time_zone"])
+    end
     super
   end
 end
