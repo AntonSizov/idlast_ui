fe05ccc9000c1c6f277ff042c813ffe63d98e247
Anton Sizov <a.sizov@dev1team.net>
2013-05-18 18:56:44 +0300

    Sort pioneers by img_id

--- app/controllers/admin/pioneers_controller.rb
+++ app/controllers/admin/pioneers_controller.rb
@@ -2,7 +2,7 @@ class Admin::PioneersController < Admin::AdminController
 
   def index
     @title = "All pioneers"
-    @pioneers = Pioneer.paginate(page: params[:page], per_page: 10)
+    @pioneers = Pioneer.where(approved: true).order_by(img_id: -1).paginate(page: params[:page], per_page: 10)
     authorize! :index, @pioneers
   end
 
