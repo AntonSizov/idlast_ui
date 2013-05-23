4c177f3ab6ad0ee0aa88cab238aeb1d060654326
Anton Sizov <a.sizov@dev1team.net>
2013-05-23 12:27:26 +0300

    Hide idlast pendings in pendings page

--- app/controllers/admin/pendings_controller.rb
+++ app/controllers/admin/pendings_controller.rb
@@ -2,7 +2,7 @@ class Admin::PendingsController < Admin::AdminController
 
   def index
     @title = "All pioneers"
-    @pioneers = Pioneer.where(approved: false).order_by(created_at: -1).paginate(page: params[:page], per_page: 10)
+    @pioneers = Pioneer.where(approved: false, :user_name.ne => "IDLast.com").order_by(created_at: -1).paginate(page: params[:page], per_page: 10)
     authorize! :index, @pioneers
   end
 
