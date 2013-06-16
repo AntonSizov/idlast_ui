957855406e1dd3a1fda493b395df0b957ba69d2f
Anton Sizov <a.sizov@dev1team.net>
2013-06-16 23:55:51 +0300

    Fix hidden comments

--- app/assets/javascripts/comments.js
+++ app/assets/javascripts/comments.js
@@ -60,8 +60,10 @@ $('#new_comment textarea').autoGrow();
 
 function hide_old_coments(){
 	all_comments = $('li.comment');
-	hide_comments = all_comments.slice(0,all_comments.length-3);
-	hide_comments.hide();
+	if (all_comments.length > 3) {
+		hide_comments = all_comments.slice(0,all_comments.length-3);
+		hide_comments.hide();
+	};
 	$('.show-all-comments-btn').on('click',function(){
 		$(this).hide();
 		hide_comments.slideDown();
